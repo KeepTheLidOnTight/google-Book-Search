0a99dec3a1cc2c97e74ba02a3666e98b01657e78
Ryan Freese <ryan.p.freese@gamil.com>
2020-12-01 19:39:19 -0600

    making README

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksControllers");
 
-// Matches with "/api/books"
+// matches with "/api/books"
 router.route("/")
   .get(booksController.findAll)
   .post(booksController.create);
 
-// Matches with "/api/books/:id"
+// match with "/api/books/:id"
 router
   .route("/:id")
   .get(booksController.findById)
